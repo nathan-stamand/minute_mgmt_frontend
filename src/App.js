f01ffc60f2e68894c543149c0d2b9cbaaca08ee8
nathan-stamand <nathansaint810@gmail.com>
2020-10-05 08:27:05 -0500

    Add fetch requests via useEffect, set tasks and subtasks to App state, create const for active / completed tasks / subtasks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NewTaskInput from "./components/NewTaskInput";
-import './App.css';
 import ActiveTasks from './containers/ActiveTasks';
 import CompletedTasks from './containers/CompletedTasks';
+import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState(null);
+  const [subTasks, setSubTasks] = useState(null);
+
+  useEffect(() => {
+    if (!tasks) {
+      fetch('http://localhost:3001/v1/api/tasks')
+      .then(res => res.json())
+      .then(json => setTasks(json.tasks));
+      fetch('http://localhost:3001/v1/api/subtasks')
+      .then(res => res.json())
+      .then(json => setSubTasks(json.subtasks));
+    }
+  });
+
+  const activeTasks = tasks ? tasks.filter(task => !task.complete) : null;
+  const activeSubTasks = subTasks ? subTasks.filter(task => !task.complete) : null;
+  const completedTasks = tasks ? tasks.filter(task => task.complete) : null;
+  const completedSubTasks = subTasks ? subTasks.filter(task => task.complete) : null;
+
   return (
     <div>
       <h1>MINUTE / MGMT</h1>
       <NewTaskInput />
-      <ActiveTasks />
-      <CompletedTasks />
+      <ActiveTasks tasks={activeTasks} subTasks={activeSubTasks} />
+      <CompletedTasks tasks={completedTasks} subTasks={completedSubTasks} />
     </div>
   );
 }
