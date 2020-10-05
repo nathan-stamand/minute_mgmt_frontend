421a0567c866e5291fa558a82426054a843c92e9
nathan-stamand <nathansaint810@gmail.com>
2020-10-05 08:30:03 -0500

    Add helper functions filterSubs and createTask

--- src/containers/ActiveTasks.js
+++ src/containers/ActiveTasks.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Task from "../components/Task";
 
-function ActiveTasks() {
+function ActiveTasks(props) {
+  function createTask(task, subs) {
+    return <Task attr={task} subTasks={subs} />
+  }
+
+  function filterSubs() {
+    
+  }
+
   return(
-    <div id="active tasks"></div>
+    <div id="active-tasks">
+
+    </div>
   )
 };
 
