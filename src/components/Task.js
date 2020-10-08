d5fe1c52e490540100bf1e3ec7275005bdc9c14c
nathan-stamand <nathansaint810@gmail.com>
2020-10-08 17:07:29 -0500

    Set up #generateSubs and the HTML for tasks to show task attribute and change completed state when double clicked

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import {v4 as uuid} from 'uuid';
 
 function Task(props) {
   const [completed, setCompleted] = useState(false);
   const [timerOn, setTimerOn] = useState(false);
 
+  function generateSubs() {
+    if (props.subTasks) {
+      return props.subTasks.map(sub => {
+        return <li key={uuid()}><input type="checkbox" id={sub.id} /><span>{sub.task}</span></li>
+      })
+    }
+  }
   return(
-    <div className="compressed task">
-      
+    <div onDoubleClick={() => setCompleted(!completed)} className="task">
+      <p>{props.attr.task}</p>
+      {generateSubs()}
     </div>
   )
 };
