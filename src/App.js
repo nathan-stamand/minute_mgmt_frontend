5b511eac6045db5c30c266f092173c39e33d3bd8
nathan-stamand <nathansaint810@gmail.com>
2020-10-09 16:39:53 -0500

    Remove references about Subtasks

--- src/App.js
+++ src/App.js
@@ -6,30 +6,24 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState(null);
-  const [subTasks, setSubTasks] = useState(null);
 
   useEffect(() => {
     if (!tasks) {
       fetch('http://localhost:3001/v1/api/tasks')
       .then(res => res.json())
       .then(json => setTasks(json.tasks));
-      fetch('http://localhost:3001/v1/api/subtasks')
-      .then(res => res.json())
-      .then(json => setSubTasks(json.subtasks));
     }
   });
 
   const activeTasks = tasks ? tasks.filter(task => !task.complete) : null;
-  const activeSubTasks = subTasks ? subTasks.filter(task => !task.complete) : null;
   const completedTasks = tasks ? tasks.filter(task => task.complete) : null;
-  const completedSubTasks = subTasks ? subTasks.filter(task => task.complete) : null;
 
   return (
     <div>
       <h1>MINUTE / MGMT</h1>
       <NewTaskInput />
-      <ActiveTasks tasks={activeTasks} subTasks={activeSubTasks} />
-      <CompletedTasks tasks={completedTasks} subTasks={completedSubTasks} />
+      <ActiveTasks tasks={activeTasks} />
+      <CompletedTasks tasks={completedTasks} />
     </div>
   );
 }
